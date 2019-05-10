4f1bf9f53768b1e82399431e80122554008d355f
annotator14 <50084663+annotator14@users.noreply.github.com>
2019-05-10 20:42:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -18,9 +18,33 @@ window.onload=function(){
   document.getElementById('contactForm').addEventListener('submit',submitForm);
 }
 
+document.getElementById('contactForm').onsubmit = function(e) {
+    // reference to select list using this keyword and form elements collection
+    // no callback function used this time
+    var Discourse1 = getInputList( this.elements['Discourse1'] );
+    
+    //alert( 'The number of options selected is: ' + opts.length ); //  number of selected options
+    
+    return false; // don't return online form
+};
 
-
-
+function getInputList(id){
+	var opts = [], opt;
+    
+    // loop through options in select list
+    for (var i=0, len=id.options.length; i<len; i++) {
+        opt = id.options[i];
+        
+        // check if selected
+        if ( opt.selected ) {
+            // add to array of option elements to return from this function
+            opts.push(opt);
+        }
+    }
+    
+    // return array containing references to selected option elements
+    return opts;
+}
 //Submit form
 function submitForm(e){
  e.preventDefault();
@@ -28,7 +52,7 @@ function submitForm(e){
 //Get values
 var Comment1=getInputVal('Comment1');
 var Comment2=getInputVal('Comment2');
-var Discourse1=getInputList(this.elements['Discourse1'] );
+//var Discourse1=getInputList(this.elements['Discourse1'] );
 var Discourse2=getInputList(this.elements['Discourse2']); 
 
 //Save message
@@ -49,23 +73,7 @@ document.getElementById('contactForm').reset();
 function getInputVal(id){
 	return document.getElementById(id).value;
 }
-function getInputList(id){
-	var opts = [], opt;
-    
-    // loop through options in select list
-    for (var i=0, len=id.options.length; i<len; i++) {
-        opt = id.options[i];
-        
-        // check if selected
-        if ( opt.selected ) {
-            // add to array of option elements to return from this function
-            opts.push(opt);
-        }
-    }
-    
-    // return array containing references to selected option elements
-    return opts;
-}
+
  
 //Save message to firebase
 function saveMessage(Comment1,Comment2,Discourse1,Discourse2){
