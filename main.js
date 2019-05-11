ea7d0c0a0d640806128ab4e0a73eb526e23edd3e
annotator14 <50084663+annotator14@users.noreply.github.com>
2019-05-11 12:31:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -18,24 +18,20 @@ var messagesRef=firebase.database().ref('Annotations3');
 
 window.onload=function(){
   document.getElementById('contactForm').addEventListener('submit',submitForm);
-  document.getElementById('contactForm1').addEventListener('submit',submitForm);
-  document.getElementById('contactForm2').addEventListener('submit',submitForm);
+  document.getElementById('contactForm2').addEventListener('submit',submitForm2);
+  document.getElementById('contactForm3').addEventListener('submit',submitForm3);
 }
 
 
 //Submit form
 function submitForm(e){
  e.preventDefault();
-//var zy=getInputList('Discourse1');
-//alert(zy);
-//Get values
 var Comment1=getInputVal('Comment1');
 var Comment2=getInputVal('Comment2');
 var Discourse1=loopSelected('Discourse1');
 var Discourse2=loopSelected('Discourse2'); 
 	
 function loopSelected(id) { 
-    //var Discourse = document.getElementById(id);
     var selectedArray = new Array();
     var selObj = document.getElementById(id); 
     var i;
@@ -67,18 +63,116 @@ document.getElementById('contactForm').reset();
 function getInputVal(id){
 	return document.getElementById(id).value;
 }
-/*
-var Discourse1 =  function(selectElement) {
-  return [].reduce.call(selectElement.options, function(result, option) {
-    if (option.selected) result.push(option.value);
-    return result;
-  }, []);
-};*/
+
  
 //Save message to firebase
-function saveMessage(Comment1,Comment2,Discourse1,Discourse2,selectedArray){
+function saveMessage(Comment1,Comment2,Discourse1,Discourse2){
 	var newMessageRef=messagesRef.push();
 	newMessageRef.set({
+		Comment1: Comment1,
+		Comment2: Comment2,
+		Discourse1: Discourse1,
+		Discourse2: Discourse2
+	});
+}
+
+function submitForm2(e){
+ e.preventDefault();
+var Comment1=getInputVal('Comment1');
+var Comment2=getInputVal('Comment2');
+var Discourse1=loopSelected('Discourse1');
+var Discourse2=loopSelected('Discourse2'); 
+	
+function loopSelected(id) { 
+    var selectedArray = new Array();
+    var selObj = document.getElementById(id); 
+    var i;
+    var count = 0;
+    for (i=0; i<selObj.options.length; i++) { 
+        if (selObj.options[i].selected) {
+            selectedArray[count] = selObj.options[i].value;
+            count++; 
+        } 
+    } 
+return selectedArray;
+}
+//loopSelected();
+//Save message
+saveMessage(Comment1,Comment2,Discourse1,Discourse2);
+
+//Show alert
+document.querySelector('.alert').style.display='block';
+
+//Hide alert after 3 seconds
+setTimeout(function(){
+	document.querySelector('.alert').style.display='none';
+},3000);
+
+//Clear form
+document.getElementById('contactForm2').reset();
+ }
+//Function to get form values
+function getInputVal(id){
+	return document.getElementById(id).value;
+}
+
+ 
+//Save message to firebase
+function saveMessage(Comment1,Comment2,Discourse1,Discourse2){
+	var newMessageRef2=messagesRef2.push();
+	newMessageRef2.set({
+		Comment1: Comment1,
+		Comment2: Comment2,
+		Discourse1: Discourse1,
+		Discourse2: Discourse2
+	});
+}
+
+function submitForm3(e){
+ e.preventDefault();
+var Comment1=getInputVal('Comment1');
+var Comment2=getInputVal('Comment2');
+var Discourse1=loopSelected('Discourse1');
+var Discourse2=loopSelected('Discourse2'); 
+	
+function loopSelected(id) { 
+    var selectedArray = new Array();
+    var selObj = document.getElementById(id); 
+    var i;
+    var count = 0;
+    for (i=0; i<selObj.options.length; i++) { 
+        if (selObj.options[i].selected) {
+            selectedArray[count] = selObj.options[i].value;
+            count++; 
+        } 
+    } 
+return selectedArray;
+}
+//loopSelected();
+//Save message
+saveMessage(Comment1,Comment2,Discourse1,Discourse2);
+
+//Show alert
+document.querySelector('.alert').style.display='block';
+
+//Hide alert after 3 seconds
+setTimeout(function(){
+	document.querySelector('.alert').style.display='none';
+},3000);
+
+//Clear form
+document.getElementById('contactForm3').reset();
+ }
+//Function to get form values
+function getInputVal(id){
+	return document.getElementById(id).value;
+}
+
+ 
+//Save message to firebase
+function saveMessage(Comment1,Comment2,Discourse1,Discourse2){
+	var newMessageRef3=messagesRef3.push();
+	newMessageRef3.set({
 		Comment1: Comment1,
 		Comment2: Comment2,
 		Discourse1: Discourse1,
