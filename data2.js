e1cbb50755c5854767dbde85378406f8863a462a
annotator14 <50084663+annotator14@users.noreply.github.com>
2019-05-12 00:57:50 +0530

    Update data2.js

--- data2.js
+++ data2.js
@@ -1,1 +1,1 @@
-data2={"phrases":[{"id":"137854","body":"Sems are too hard to handle!"},{"id":"137854","body":"Sems are too hard to handle!"},{"id":"137854","body":"Sems are too hard to handle!"}]}
+data2={"phrases":[{"id":"137854","body":"Sems are too hard to handle!"},{"id":"137854","body":"Sems are too hard to handle 2!"},{"id":"137854","body":"Sems are too hard to handle 3!"}]}
