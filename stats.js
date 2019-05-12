ffe68e8fd52ad1a38cfabb2ad4408225c5a498a3
annotator14 <50084663+annotator14@users.noreply.github.com>
2019-05-12 18:07:38 +0530

    Update stats.js

--- stats.js
+++ stats.js
@@ -5,13 +5,27 @@ class Complete {
         this.com_in3 = com_in3;
     }
 
-    get_level (com_in1,com_in2,com_in3) {
-        return this.com_in1,this.com_in2,this.com_in3;
+    get_level1 (com_in1) {
+        return this.com_in1;
     }
 
-    set_level (com_in1,com_in2,com_in3) {
+    set_level1 (com_in1) {
         this.com_in1 = com_in1;
+    }
+    get_level2 (com_in2) {
+        return this.com_in2;
+    }
+
+    set_level2 (com_in2) {
+        
         this.com_in2 = com_in2;
+        
+    }
+    get_level3 (com_in3) {
+        return this.com_in3;
+    }
+
+    set_level3 (com_in3) {
         this.com_in3 = com_in3;
     }
 }
